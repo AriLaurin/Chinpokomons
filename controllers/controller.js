718425bb56962085cd68de362a5de5078891b6a7
AriLaurin <arijuslaurin@gmail.com>
2023-05-09 10:15:36 +0200

    Added Home Route

---
+++ controllers/controller.js
@@ -0,0 +1,3 @@
+module.exports.home_get = (req,res) => { //a function that renders our routes from AuthRoutes
+    res.render("home");
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const Routes = require("./routes/routes"); //importing the routes so our app actually uses them
+const cookieParser = require("cookie-parser");
+const {checkUser, requireAuth} = require("./middleware/middleware");
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json()); //takes any json data from requests, and parses it into a js code
+app.use(cookieParser()) // we can access cookie objects
+
+// view engine
+app.set('view engine', 'ejs');
+
+const dbURI = process.env.host
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(80))
+  .then(console.log("DATABASE STATUS: CONNECTED"))
+  .then(console.log("SERVER STATUS: ONLINE"))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser); // * means every route
+// app.get('/create', requireAuth, (req, res) => res.render('create'));
+app.use(Routes);
